32dbb890a5f5759a5b0353133689032775865210
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2026-05-05 03:24:14 -0400

    added add_serial_point

--- jupiterli/__init__.py
+++ jupiterli/__init__.py
@@ -3,6 +3,10 @@ import redis
 REDIS_URL = "redis://localhost"
 redis_conn = redis.from_url(REDIS_URL, decode_responses=True)
 
+def add_serial_point(key, value):
+    global redis_conn
+    redis_conn.xadd(key, {"value": value}, maxlen = 10000)    
+
 def add_ts_point(key, ts, value):
     global redis_conn
     redis_conn.xadd(key, {"timestamp": ts, "value": value}, maxlen = 10000)
