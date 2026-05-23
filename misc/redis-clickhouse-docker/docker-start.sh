642fabc563541b4bed5c75cc62bf9b4c9db3247c
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2026-05-22 20:00:38 -0400

    added autostart of intake server in container

--- misc/redis-clickhouse-docker/docker-start.sh
+++ misc/redis-clickhouse-docker/docker-start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+
 echo "starting redis as daemon"
 redis-server --bind 0.0.0.0 --protected-mode no --daemonize yes
 
 echo "running clickhouse entrypoint.sh"
-exec ./entrypoint.sh
+./entrypoint.sh &
+
+echo "creating venv"
+python3 -m venv /venv
+echo "installing intake script deps"
+/venv/bin/pip install redis clickhouse-driver
+echo "running intake script"
+exec /venv/bin/python /redis-clickhouse-intake.py >& /redis-clickhouse-intake.py.log
+
